59974c3e83a0caad8c47a531c3274f292601000b
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-16 11:03:04 -0600

    firs push

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const test = prompt ("Do you really wanna see this site?")
+let answer = 'no'
+
+const tryTest = () => {
+    if (test == answer){
+        alert("congrats!")
+    } else {
+        alert ("AWh man its opposite day yo! I'll let you in anyway.")
+    }
+}
+tryTest ()
+
+console.log("hello world");
+const numFour = 2 + 2;
+console.log(numFour);
+
+const groceriesArticle = document.querySelector('#groceries');
+groceriesArticle.classList.add('new-text')
+
+const groceriesHeader = document.querySelector('.groceries__header');
+// groceriesHeader.classList.remove("groceries__header");
+
+
+const groceryList = document.querySelectorAll('#grocery-list li');
+console.log(groceryList);
+for (let i=0; i<groceryList.length; i++){
+    if (i===1){
+    groceryList[i].classList.add("groceries__header");
+}
+}
+
+
+const addedFood = []
+const addItem = () => {
+    let newFood = document.querySelector('.new-item').value;
+    addedFood.unshift(newFood);
+    // console.log(addedFood)
+    for (let i=0; i<1; i++){
+        let newLI = document.body.createElement('LI');
+        const newHTML =`${addedFood[i]}`;
+        newLI.innerHTML = newHTML;
+    
+    }
+
+}
